244bdad924b92bbd67a5d236f1dd88169fae2f7c
Nathaniel Evans <nathaniel.evansj@gmail.com>
2018-10-22 17:13:34 -0700

    commenting could be done, or could be fleshed out a bit. Still need results write up

--- HW4/evans_hw4.py
+++ HW4/evans_hw4.py
@@ -6,50 +6,38 @@ Created on Mon Oct 22 13:54:51 2018
 @class: BMI665 
 @HW: 4 
 
-
-Assignment #4
-Submit source code and write-up (including program output) through Sakai.
-BMI 565/665 Bioinformatics Programming and Scripting
-1) Create a custom exception definition that can be raised when a sequence is something other than
-DNA (e.g., an mRNA or protein sequence). Call this exception "SeqTypeError".
-2) Using either lxml or BeautifulSoup, extract the nucleotides represented by the IUPAC nucleotide
-codes W, S, M, K, R, and Y from the table in the following Wikipedia page (assume you know that N
-means any nucleotide: A, T, C or G). Store this information in a dictionary and use the info to create
-the regular expressions for the recognition sites listed in (3) below:
-https://en.wikipedia.org/wiki/Nucleic_acid_notation
-3) Create a Class definition for DNA sequence data with the following features:
-- a class attribute called ResEnzymeDict, which is a dictionary of restriction enzymes and their
-recognition sites (as regular expression patterns).
-- an instance attribute to hold a DNA sequence
-- The user will be alerted if the sequence is not DNA (using the exception created above).
-- a method, called "restriction_sites" that searches the DNA sequence for the restriction enzyme
-recognition sites (using regular expressions), and prints out those enzymes whose recognition sites
-exist in the DNA sequence along with the sequence that matched the recognition site pattern.
-The ResEnzymeDict attribute should contain the following enzymes and recognition sites:
-EcoRI: GAATTC
-EalI: YGGCCR
-ErhI: CCWWGG
-EcaI: GGTNACC
-FblI: GTMKAC
-Deliverables:
-- Code for 1, 2 and 3 in a single file
-- Write-up of results
+This program can be run from bash command line using the command: 
+    $ python evans_hw4.py <DNA-seq-string> 
+    eg. 
+    $ python evans_hw4.py "GATTACA" 
+    
+    Alternatively, running without an argument will result in a default DNA seq test. 
+    
+    The purpose of this script is to:
+        A. Read in the IUPAC codes from the url: 
+            https://en.wikipedia.org/wiki/Nucleic_acid_notation
+        B. Analyze a user provided DNA sequence and print to console any restriction enzyme sites that are found. 
+        
+    If the provided DNA sequence contains characters other than [ATCG] a SeqTypeError will be thrown. 
+            
 """
 
 from bs4 import BeautifulSoup 
 import urllib.request as url 
 import re
+import sys
 
+#url to extract pattern conversion data 
 WIKI_URL = "https://en.wikipedia.org/wiki/Nucleic_acid_notation" 
 
+# exception to be thrown if a DNA sequence representation is misrepresented
 class SeqTypeError(Exception): 
     '''Sequence is not valid DNA representation''' 
     pass 
     
-
 def parse_table(table, kbs_to_extract): 
     '''
-    return set of kbs that it represents and the complement
+    Given a table, extract the desired information and return as a dictionary. 
     
     input
         table <bs4.element.tag> beautiful soup object representing the table to be parsed 
@@ -73,6 +61,10 @@ def parse_table(table, kbs_to_extract):
 
 
 class DNA_SEQ: 
+    ''' 
+    This class represents a DNA sequence and can be used to find any given matching Restriction Enzyme sites. A dictionary condex is needed to convert IUPAC codes to regular expression patterns, should you wish to interpret these patterns differently, a custom codex dictionary can be passed in.
+    '''
+    
     ResEnzymeDict = {
             'EcoRI': 'GAATTC',
             'EalI': 'YGGCCR',
@@ -82,12 +74,31 @@ class DNA_SEQ:
             }
     
     def __init__(self, DNA, codex): 
-        if not re.sub('[ATCG]', '', DNA) == '': raise SeqTypeError()
+        '''
+        initialize the DNA_SEQ object
+        
+        inputs 
+            DNA<str> sequence representing one strand of DNA, must only include characters [ATCG]
+            codex<dict> dictionary mapping IUPAC codes to re expressions, for syntax, use help(parse_table)
+            
+        outputs
+            None
+        '''
+
+        if not re.sub('[ATCG]', '', DNA) == '': raise SeqTypeError() # make sure only ATCG values are present in seq
         
         self.codex = codex # extracted table info 
         self.DNA = DNA 
     
     def __generate_re_patterns__(self): 
+        '''
+        convert IUPAC restriction enzyme patterns to RE pattern using instance provided codex. 
+        
+        inputs
+            None
+        outputs
+            None
+        '''
         self.ResEnzymeRE = {}
         for name in self.ResEnzymeDict: 
             codified_pattern = self.ResEnzymeDict[name]
@@ -95,10 +106,18 @@ class DNA_SEQ:
             for kb in codified_pattern: 
                 pattern += "[" + set_to_string(self.codex[kb]['base_rep']) + "]"
             self.ResEnzymeRE[name] = pattern
-        
-        return self.ResEnzymeRE
+
     
     def restriction_sites(self): 
+        '''
+        Searches instance DNA sequence for subsequences that match the provided IUPAC patterns. When a match is found, it is printed to console. 
+        
+        inputs 
+            None
+        
+        ouputs
+            None
+        '''
         self.__generate_re_patterns__()
         for name in self.ResEnzymeRE: 
             srch = re.finditer(self.ResEnzymeRE[name],self.DNA)
@@ -110,20 +129,32 @@ class DNA_SEQ:
                 output+='DNA sequence match: ' + str(find.group(0)) + '\n'
             output += '-------------------------------------------------------'
             
-            # had a very difficult time finding any sleek way to check if an interable object is empty or not, so this is my work around to only print the objects that are not empty. Bit wasteful, but easy implementation. 
-            if (len(output) > 146) : 
+            # had a very difficult time finding any sleek way to check if an interable object is empty or not, so this is my work around to only print the objects that are not empty. It's a bit wasteful, but easy to implement. 
+            
+            if (len(output) > 175) : 
                 print(output)
 
 def set_to_string(_set): 
+    '''
+    convert set of chars to string without syntax
+    
+    input
+        _set<set> chars to be converted to string 
+        
+    output
+        s<str> string of set chars
+    '''
+    
     s = ''
     for item in _set: 
         s += str(item)  
     return s
     
 
-
-if __name__ == "__main__" : 
-    # main script 
+'''
+executed script 
+'''
+if __name__ == "__main__" :  
     
     ## part 2, extract table and store info 
     html = url.urlopen(WIKI_URL).read() # pull html and read into memory 
@@ -131,13 +162,20 @@ if __name__ == "__main__" :
     NAN.prettify() 
     tables = NAN.find_all('table')
 
-    # parse the data and add intuitive knowledge about N 
+    # parse relevant data
     tbl_data = parse_table(tables[0], {'W', 'S', 'M', 'K', 'R', 'Y', 'N', 'A', 'C', 'G', 'T'} )
-    print(tbl_data)
     
-    mySeq = DNA_SEQ('GATTACAGAATTCTTTTTTGAATTC',tbl_data)
+    # seq data input method 
+    if (len(sys.argv) > 1): 
+        DNA = sys.argv[1]
+    else: #    EcoRI ------EalI --------ErhI ---------- EcalI -------- FblI
+        DNA = "GAATTCAAAAAACGGCCAAAAAAAAACCTTGGAAAAAAAAGGTAACCAAAAAAAAAGTATAC"
+        print('test case, seq used: ' + DNA)
+            
+    # anaylze data
+    mySeq = DNA_SEQ(DNA, tbl_data)
     mySeq.restriction_sites()
-    print(mySeq.ResEnzymeRE)
+
     
             
     
