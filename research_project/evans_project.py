d43bd6d7c4cc0c6b7fd856d92ff7f20f90da3db8
Nathaniel Evans <nathaniel.evansj@gmail.com>
2018-11-21 17:07:10 -0800

    took a stab at P2

--- research_project/evans_project.py
+++ research_project/evans_project.py
@@ -6,16 +6,29 @@ Created on Tue Nov 20 10:12:14 2018
 @author: nathaniel evans 
 @class: BMI665 
 
+
+odds ratio: https://sakai.ohsu.edu/access/content/group/BMI-565-665-DL-F18/Week%207/research_project_background_BMI565.pdf 
+
+Project Description: https://sakai.ohsu.edu/access/content/group/BMI-565-665-DL-F18/Week%207/research_project_description_BMI565.pdf
+
+kegg: https://www.genome.jp/kegg/pathway.html
+https://www.genome.jp/dbget-bin/www_bget?pathway:map05222
 """
 
 data_path = './data/BMI565_ResearchProject_Data/'
 from matplotlib import pyplot as plt 
-import seaborn as sns 
+from Bio import Entrez 
+from Bio.Align.Applications import ClustalwCommandline
+import sys 
+
+sys.path.append('/c/ProgramData/Start Menu/Programs/ClustalW2') # can't get the clustalw2 to runnnn
 
 
 class pathway: 
     
     def __init__(self, raw, DE, allPB): 
+        Entrez.email = 'evansna@ohsu.edu' 
+        
         self.ID, self.name, self.group = self.parse(raw)
         
         self.DE_genes = self.group.intersection(DE) # A
@@ -24,7 +37,7 @@ class pathway:
         self.nonPathway_genes = allPB - self.group
         self.nonPathway_DE_genes = self.nonPathway_genes.intersection(DE) # C
         self.nonPathway_nonDE_genes = self.nonPathway_genes - self.nonPathway_DE_genes # D 
-        
+
         self.odds_ratio = (len(self.DE_genes) * len(self.nonPathway_nonDE_genes)) / (len(self.nonDE_genes) * len(self.nonPathway_DE_genes))
     
     def parse(self, raw): 
@@ -41,6 +54,63 @@ class pathway:
     
     def __lt__(self, other): 
         return (self.odds_ratio < other.odds_ratio)
+
+    def get_gene_seq(self, gene_sym, species): 
+        # returns fasta file text 
+        
+        
+        assert species.lower() in set(['homo sapiens', 'mus musculus', 'canis lupus familiaris']), 'improper species input' 
+        
+        try: 
+            
+            search_handle = Entrez.esearch(db='nuccore',term= gene_sym + '[Gene Name] AND ' + species + '[organism]')
+            record = Entrez.read(search_handle)
+            ids = record["IdList"]
+            print('ids', ids)
+            
+            result = Entrez.efetch(db="nuccore", id=ids[0], rettype="fasta", retmode="xml")
+            text = Entrez.read(result)[0]
+            
+            assert text['TSeq_orgname'].lower() == species.lower(), 'wrong species returned! Wanted: ' + species + ', received: ' + text['TSeq_orgname'] 
+            
+            return Entrez.efetch(db="nuccore", id=ids[0], rettype="fasta", retmode="txt").read()
+        
+        except: 
+            raise #entrez_search_failure
+            
+        
+    def get_edit_distances(self, gene): 
+        
+        species = ['homo sapiens', 'mus musculus', 'canis lupus familiaris'] 
+        
+        for gene in self.group: 
+            
+            # write fasta file to disk for comparison 
+            human_fasta = self.get_gene_seq(gene, species[0])
+            mouse_fasta = self.get_gene_seq(gene, species[1])
+            dog_fasta = self.get_gene_seq(gene, species[2])
+            
+            full_fasta = human_fasta + '\n' + mouse_fasta + '\n' + dog_fasta
+
+            with open('./temp/unaligned.fasta', 'w') as f: 
+                f.write(full_fasta)
+                
+            # run clustalw  # ~/Documents/BMI665/BMI665/research_project
+            command = ClustalwCommandline("clustalw2", infile="./temp/unaligned.fasta")
+            stdout, stderr = command()
+            
+            # read alignment file into ram 
+            print(stdout)
+            # calculate edit distance 
+            
+            # save human-mouse e-dist, human-canis e-dist 
+            
+            
+    
+class entrez_search_failure(Exception): 
+    
+    def __str__(self): 
+        return 'the entrez gene search failed to return a valid response' 
 
 if __name__ == '__main__' : 
     
@@ -88,7 +158,7 @@ if __name__ == '__main__' :
     ys = list(map(lambda x: x.odds_ratio, sorted_pathways))
     xs = list(range(len(ys)))
     
-    plt.plot(xs, ys)
+    #plt.plot(xs, ys)
     
     tep = sorted_pathways[0] # top expression pathway (TEP)
     
@@ -97,7 +167,12 @@ if __name__ == '__main__' :
     print('name:', tep.name)
     print('group:', tep.group)
     
+    #print(tep.get_gene_seq(list(tep.group)[0], species = 'homo sapiens'))
     
+    # species : Homo Sapiens, mus musculus, canis lupus
+    
+    
+    tep.get_edit_distances(list(tep.group)[0]) 
     
 
     
